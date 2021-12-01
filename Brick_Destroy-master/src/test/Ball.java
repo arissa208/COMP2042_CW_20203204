445bfc91eccd75bdb2e85f45d7e221d56e508243
Nurul Arissa <nurularissabh@gmail.com>
2021-12-01 08:28:21 +0800

    Making Ball fields final to ensure fully initialisation

--- Brick_Destroy-master/src/test/Ball.java
+++ Brick_Destroy-master/src/test/Ball.java
@@ -10,20 +10,20 @@ abstract public class Ball {
     // Attributes of ball
     private Shape ballFace;
 
-    private Point2D center;
+    private final Point2D center;
     Point2D up;
     Point2D down;
     Point2D left;
     Point2D right;
 
-    private Color border;
-    private Color inner;
+    private final Color border;
+    private final Color inner;
 
     private int speedX;
     private int speedY;
 
     // Constructor of ball class
-    public Ball(Point2D center,int radiusA,int radiusB,Color inner,Color border){
+    public Ball(Point2D center, int radiusA, int radiusB, Color inner, Color border) {
         this.center = center;
 
         up = new Point2D.Double();
@@ -31,89 +31,100 @@ abstract public class Ball {
         left = new Point2D.Double();
         right = new Point2D.Double();
 
-        up.setLocation(center.getX(),center.getY()-(radiusB / 2));
-        down.setLocation(center.getX(),center.getY()+(radiusB / 2));
+        up.setLocation(center.getX(), center.getY() - (radiusB / 2));
+        down.setLocation(center.getX(), center.getY() + (radiusB / 2));
 
-        left.setLocation(center.getX()-(radiusA /2),center.getY());
-        right.setLocation(center.getX()+(radiusA /2),center.getY());
+        left.setLocation(center.getX() - (radiusA / 2), center.getY());
+        right.setLocation(center.getX() + (radiusA / 2), center.getY());
 
 
-        ballFace = makeBall(center,radiusA,radiusB);
+        ballFace = makeBall(center, radiusA, radiusB);
         this.border = border;
-        this.inner  = inner;
+        this.inner = inner;
         speedX = 0;
         speedY = 0;
     }
 
     // makeBall abstract method
-    protected abstract Shape makeBall(Point2D center,int radiusA,int radiusB);
+    protected abstract Shape makeBall(Point2D center, int radiusA, int radiusB);
 
     // movement methods
-    public void move(){
+    public void move() {
         RectangularShape tmp = (RectangularShape) ballFace;
-        center.setLocation((center.getX() + speedX),(center.getY() + speedY));
+        center.setLocation((center.getX() + speedX), (center.getY() + speedY));
         double w = tmp.getWidth();
         double h = tmp.getHeight();
 
-        tmp.setFrame((center.getX() -(w / 2)),(center.getY() - (h / 2)),w,h);
-        setPoints(w,h);
+        tmp.setFrame((center.getX() - (w / 2)), (center.getY() - (h / 2)), w, h);
+        setPoints(w, h);
 
 
         ballFace = tmp;
     }
-    public void reverseX(){
+
+    public void reverseX() {
         speedX *= -1;
     }
-    public void reverseY(){
+
+    public void reverseY() {
         speedY *= -1;
     }
-    public void moveTo(Point p){
+
+    public void moveTo(Point p) {
         center.setLocation(p);
 
         RectangularShape tmp = (RectangularShape) ballFace;
         double w = tmp.getWidth();
         double h = tmp.getHeight();
 
-        tmp.setFrame((center.getX() -(w / 2)),(center.getY() - (h / 2)),w,h);
+        tmp.setFrame((center.getX() - (w / 2)), (center.getY() - (h / 2)), w, h);
         ballFace = tmp;
     }
 
     // setter for private fields
-    public void setSpeed(int x,int y){
+    public void setSpeed(int x, int y) {
         speedX = x;
         speedY = y;
     }
-    public void setXSpeed(int s){
+
+    public void setXSpeed(int s) {
         speedX = s;
     }
-    public void setYSpeed(int s){
+
+    public void setYSpeed(int s) {
         speedY = s;
     }
-    private void setPoints(double width,double height){
-        up.setLocation(center.getX(),center.getY()-(height / 2));
-        down.setLocation(center.getX(),center.getY()+(height / 2));
 
-        left.setLocation(center.getX()-(width / 2),center.getY());
-        right.setLocation(center.getX()+(width / 2),center.getY());
+    private void setPoints(double width, double height) {
+        up.setLocation(center.getX(), center.getY() - (height / 2));
+        down.setLocation(center.getX(), center.getY() + (height / 2));
+
+        left.setLocation(center.getX() - (width / 2), center.getY());
+        right.setLocation(center.getX() + (width / 2), center.getY());
     }
 
     // getter for private fields
-    public Color getBorderColor(){
+    public Color getBorderColor() {
         return border;
     }
-    public Color getInnerColor(){
+
+    public Color getInnerColor() {
         return inner;
     }
-    public Point2D getPosition(){
+
+    public Point2D getPosition() {
         return center;
     }
-    public Shape getBallFace(){
+
+    public Shape getBallFace() {
         return ballFace;
     }
-    public int getSpeedX(){
+
+    public int getSpeedX() {
         return speedX;
     }
-    public int getSpeedY(){
+
+    public int getSpeedY() {
         return speedY;
     }
 
